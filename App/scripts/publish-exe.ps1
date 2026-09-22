17c8093feb8b3f4707c0e0c600e9cc1a12f9f9f7
Amir <sabirovv17@users.noreply.github.com>
2026-09-22 15:42:51 +0300

    chore(posm): prepare 1.0.63 merchandising release

--- App/scripts/publish-exe.ps1
+++ App/scripts/publish-exe.ps1
@@ -13,7 +13,7 @@
 
 param(
   [string]$ConfigPath = "C:\Epharm\posm.json",
-  [string]$Version = "1.0.61",
+  [string]$Version = "1.0.63",
   [string]$OutputDir = "",
   [switch]$KeepPackageFolder
 )
